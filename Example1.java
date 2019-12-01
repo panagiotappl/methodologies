5710a6f63fcacefb208f12dbdf3dc4196a83d5ec
panagiotappl <panagiotappl@gmail.com>
2019-12-01 13:23:27 +0100

    Overriding/overloading example1

---
+++ Example1.java
@@ -0,0 +1,41 @@
+class A {
+    public void meth(A a) { System.out.println("A: meth(A a)"); }
+    public void meth(D d) { System.out.println("A: meth(D d)"); }
+    public void meth(E e) { System.out.println("A: meth(E e)"); }
+}
+
+class B extends A {
+    public void meth(A a) { System.out.println("B: meth(A a)"); }
+    public void meth(B b) { System.out.println("B: meth(B b)"); }
+    public void meth(D d) { System.out.println("B: meth(D d)"); }
+    public void meth(E e) { System.out.println("B: meth(E e)"); }
+}
+
+class C extends A {
+    public void meth(A a) { System.out.println("C: meth(A a)"); }
+    public void meth(C c) { System.out.println("C: meth(C c)"); }
+}
+
+class D {
+    
+}
+
+class E extends D {
+    
+}
+
+public class Example1 {
+     public static void main(String []args){
+        B b = new B();
+        A a = b;
+        a.meth(a);
+        a.meth(b);
+        
+        C c = new C();
+        E e = new E();
+        D d = e;
+        
+        ((A) c).meth(a);
+        c.meth((A) c);
+     }
+}
